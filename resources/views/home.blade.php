5dfbbef8769924b0e0328ed61f89112293ead000
truongtv <tranviettruong1998@gmail.com>
2021-09-18 10:27:25 +0700

    a

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="vi">
+<head>
+    <base href="{{asset('')}}">
+    <meta id="token" name="csrf-token" content="{{csrf_token()}}">
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    {{-- <meta http-equiv="refresh" content="5;url=https://vicard.vn/"> --}}
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="VICARD - Danh thiếp điện tử">
+    <meta name="geo.region" content="VN-HN">
+    <meta name="geo.placename" content="Hà Nội">
+    <meta name="geo.position" content="21.029165;105.803438">
+    <meta name="ICBM" content="21.029165, 105.803438">
+    <meta name="description" content="VICARD cung cấp danh thiếp điện tử và nền tảng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm"/>
+    <meta name="og:title" content="VICARD - Nền tảng kết nối chuyên nghiệp, hiện đại"> 
+    <meta name="og:url" content="https://vicard.vn">
+    <meta name="og:description" content="VICARD cung cấp danh thiếp điện tử và nền tảng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm">
+    <meta name="og:image" content="{{asset('logo.png')}}">
+    <link rel="shortcut icon" href="{{asset('icon.png')}}" type="image/x-icon" />
+    <title>Danh Thiếp Điện Tử đầu tiên tại Việt Nam</title>
+    <link rel="canonical" href="https://vicard.vn/" />
+</head>
+<body>
+    <div id="app">
+        website đang phát triển, xin cảm ơn !
+    </div>
+</body>
+</html>
