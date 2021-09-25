df2edf0a6e441f6588736dea6927e5eb4bc5b397
truongtv <tranviettruong1998@gmail.com>
2021-09-25 19:41:11 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,10 @@
     <meta name="geo.position" content="21.029165;105.803438">
     <meta name="ICBM" content="21.029165, 105.803438">
     <meta name="description" content="Vicard cung cấp danh thiếp điện tử và nền tảng giúp người dùng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm thẻ trên điện thoại thông minh"/>
-    <meta name="keywords" content="vicard, nfc, danh thiếp điện tử">
+    <meta name="keywords" content="vicard, nfc, danh thiếp điện tử, thẻ">
 
     <meta property="og:type" content="website">
+    <meta property="og:site_name" content="vicard">
     <meta property="og:title" content="VICARD - Danh thiếp điện tử đầu tiên tại Việt Nam">
     <meta property="og:url" content="{{asset('')}}">
     <meta property="og:description" content="Vicard cung cấp danh thiếp điện tử và nền tảng giúp người dùng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm thẻ trên điện thoại thông minh">
