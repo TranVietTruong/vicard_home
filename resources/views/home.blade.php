1a07a53a69a5f58ee7fc95230d0dfb05cb65f592
truongtv <tranviettruong1998@gmail.com>
2021-09-25 18:57:21 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,14 +13,20 @@
     <meta name="geo.position" content="21.029165;105.803438">
     <meta name="ICBM" content="21.029165, 105.803438">
     <meta name="description" content="Vicard cung cấp danh thiếp điện tử và nền tảng giúp người dùng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm thẻ trên điện thoại thông minh"/>
-    <meta name="og:title" content="VICARD - Danh thiếp điện tử đầu tiên tại Việt Nam">
-    <meta name="og:url" content="{{asset('')}}">
-    <meta name="og:description" content="Vicard cung cấp danh thiếp điện tử và nền tảng giúp người dùng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm thẻ trên điện thoại thông minh">
-    <meta name="og:image" content="{{asset('logo.png')}}">
-    <meta name="twitter:title" content="VICARD - Danh thiếp điện tử đầu tiên tại Việt Nam">
-    <meta name="twitter:url" content="{{asset('')}}">
-    <meta name="twitter:description" content="Vicard cung cấp danh thiếp điện tử và nền tảng giúp người dùng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm thẻ trên điện thoại thông minh">
-    <meta name="twitter:image" content="{{asset('logo.png')}}">
+    <meta name="keywords" content="vicard, nfc, danh thiếp điện tử">
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="VICARD - Danh thiếp điện tử đầu tiên tại Việt Nam">
+    <meta property="og:url" content="{{asset('')}}">
+    <meta property="og:description" content="Vicard cung cấp danh thiếp điện tử và nền tảng giúp người dùng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm thẻ trên điện thoại thông minh">
+    <meta property="og:image" content="{{asset('logo.png')}}">
+
+    <meta property="twitter:type" content="website">
+    <meta property="twitter:title" content="VICARD - Danh thiếp điện tử đầu tiên tại Việt Nam">
+    <meta property="twitter:url" content="{{asset('')}}">
+    <meta property="twitter:description" content="Vicard cung cấp danh thiếp điện tử và nền tảng giúp người dùng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm thẻ trên điện thoại thông minh">
+    <meta property="twitter:image" content="{{asset('logo.png')}}">
+
     <link rel="shortcut icon" href="{{asset('icon.png')}}" type="image/x-icon" />
     <title>Danh Thiếp Điện Tử đầu tiên tại Việt Nam</title>
     <link rel="canonical" href="{{asset('')}}" />
