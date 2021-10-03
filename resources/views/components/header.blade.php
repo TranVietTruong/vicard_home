63916408a2d9e0e82ea1c47ccf27d7970f619eef
truongtv <tranviettruong1998@gmail.com>
2021-10-03 16:56:45 +0700

    a

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -24,10 +24,10 @@
             </div>
             <div class="menu_mobile">
                 <div onclick="showMenu()" id="icon_show" class="icon_show">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 18v1h-24v-1h24zm0-6v1h-24v-1h24zm0-6v1h-24v-1h24z" fill="#1040e2"/><path d="M24 19h-24v-1h24v1zm0-6h-24v-1h24v1zm0-6h-24v-1h24v1z"/></svg>
                 </div>
                 <div onclick="hideMenu()" id="icon_hide" class="icon_hide">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z"/></svg>
                 </div>
             </div>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,10 +81,10 @@
                 </div>
                 <div class="menu_mobile">
                     <div onclick="showMenu()" id="icon_show" class="icon_show">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
+                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 18v1h-24v-1h24zm0-6v1h-24v-1h24zm0-6v1h-24v-1h24z" fill="#1040e2"/><path d="M24 19h-24v-1h24v1zm0-6h-24v-1h24v1zm0-6h-24v-1h24v1z"/></svg>
                     </div>
                     <div onclick="hideMenu()" id="icon_hide" class="icon_hide">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
+                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z"/></svg>
                     </div>
                 </div>
             </div>
@@ -261,68 +261,7 @@
             </div>
         </div>
     </section>
-    <footer id="footer">
-        <div class="container">
-            <div class="row">
-                <div class="five columns">
-                    <div class="head">vicard.vn</div>
-                    <div class="description">
-                        Vicard cung cấp danh thiếp điện tử và nền tảng công nghệ giúp người dùng tích hợp tất cả thông tin cần thiết mà người dùng sử dụng để Trao Đổi và Kết Nối chỉ với 1 lần chạm thẻ trên điện thoại thông minh.
-                    </div>
-                    <ul class="diachia">
-                        <li>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z"/></svg>
-                            <span>0333100001</span>
-                        </li>
-                        <li>
-                            <svg style="transform: scale(0.8);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/></svg>
-                            <span>vicard.vn@gmail.com</span>
-                        </li>
-                        <li>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>
-                            <span>Tòa nhà TechnoSoft, ngõ 15, phố Duy Tân, Cầu Giấy, Hà Nội</span>
-                        </li>
-                    </ul>
-                </div>
-                <div class="four columns">
-                    <div class="head">
-                        Thông tin khác
-                    </div>
-                    <ul>
-                        <li>
-                            <a href="/chinh-sach-bao-mat">Chính sách bảo mật</a>
-                        </li>
-                        <li>
-                            <a href="/dieu-khoan-dich-vu">Điều khoản dịch vụ</a>
-                        </li>
-                        <li>
-                            <a href="/chinh-sach-doi-tra">Chính sách đổi trả</a>
-                        </li>
-                        <li>
-                            <a href="/giao-hang-va-thanh-toan">Giao hàng và thanh toán</a>
-                        </li>
-                        <li>
-                            <a href="/thong-tin-chuyen-khoan">Thông tin chuyển khoản</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="three columns">
-                    <div class="head">
-                        Hướng dẫn
-                    </div>
-                    <ul>
-                        <li>
-                            <a href="/huong-dan-thay-doi-thong-tin">Hướng dẫn thay đổi thông tin</a>
-                        </li>
-                        <li>
-                            <a href="/thiet-bi-tuong-thich">Thiết bị tương thích</a>
-                        </li>
-                        <li>Câu hỏi thường gặp</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+    @include('components/footer')
 
     <script>
         function changeText()
